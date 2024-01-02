f6b9d40ba8bb700d6a67b4b3906bc6b34aad7b50
Don <71207900+VaperinaDEV@users.noreply.github.com>
2024-01-02 12:33:44 +0100

    Update and rename composer-fullscreen-button-mobile.js to composer-fullscreen-mobile.js

--- javascripts/discourse/initializers/composer-fullscreen-mobile.js
+++ javascripts/discourse/initializers/composer-fullscreen-mobile.js
@@ -2,12 +2,12 @@ import { withPluginApi } from "discourse/lib/plugin-api";
 import discourseComputed from "discourse-common/utils/decorators";
 
 export default {
-  name: "composer-fullscreen-button-mobile",
+  name: "composer-fullscreen-mobile",
 
   initialize() {
     withPluginApi("0.1", (api) => {
       api.modifyClass("component:composer-toggles", {
-        pluginId: "composer-fullscreen-button-mobile",
+        pluginId: "composer-fullscreen-mobile",
 
         @discourseComputed("disableTextarea")
         showFullScreenButton(disableTextarea) {
